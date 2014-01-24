d4b4c4efba5c576e35894be56c1386499a8bd671
doppl3r <deben3@gmail.com>
2014-01-24 15:56:57 -0700

    first commit

---
+++ src/Player.java
@@ -0,0 +1,73 @@
+import textures.SpriteSheet;
+
+import java.awt.*;
+
+public class Player {
+    private SpriteSheet sprite;
+    private double x;
+    private double y;
+    private double xSpeed;
+    private double ySpeed;
+
+    private int score;
+
+    private boolean up;
+    private boolean down;
+    private boolean left;
+    private boolean right;
+
+    public Player(){
+        sprite = new SpriteSheet(Window.tt.toon,8,4,0.2);
+        //sprite.resize(64,64);
+        sprite.center();
+        sprite.animate(16); //start looking down
+
+        x = Window.getOriginalWidth()/2;
+        y = Window.getOriginalHeight()/2;
+        xSpeed = ySpeed = 1.0; //0.5 works
+    }
+    public void draw(Graphics2D g){
+        sprite.draw(g);
+    }
+    public void update(double mod){
+        //animations
+        if (up) sprite.animate(0,8,mod);
+        else if (right) sprite.animate(8,16,mod);
+        else if (down) sprite.animate(16,24,mod);
+        else if (left) sprite.animate(24,32,mod);
+        //direction
+        if (up) y-=ySpeed*mod;
+        if (down) y+=ySpeed*mod;
+        if (left) x-=xSpeed*mod;
+        if (right) x+=xSpeed*mod;
+        //keep in bounds
+        if (y < 0) y = 0;
+        else if (y > Window.getOriginalHeight()) y = Window.getOriginalHeight();
+        if (x < 0) x = 0;
+        else if (x > Window.getOriginalWidth()) x = Window.getOriginalWidth();
+        //update the x value to the sprite
+        sprite.update(x,y);
+    }
+    public void moveUp(boolean up){
+        if (!up) sprite.animate(0);
+        this.up=up;
+    }
+    public void moveRight(boolean right){
+        if (!right) sprite.animate(8);
+        this.right=right;
+    }
+    public void moveDown(boolean down){
+        if (!down) sprite.animate(16);
+        this.down=down;
+    }
+    public void moveLeft(boolean left){
+        if (!left) sprite.animate(24);
+        this.left=left;
+    }
+    public void addPoint(){ score++; }
+    public int getScore(){ return score; }
+    public int getWidth(){ return sprite.getSpriteWidth(); }
+    public int getHeight(){ return sprite.getSpriteHeight(); }
+    public double getX(){ return x; }
+    public double getY(){ return y; }
+}
---
+++ src/Window.java
@@ -0,0 +1,94 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+import textures.Textures;
+
+/**
+ */
+public class Window {
+    static JFrame jf;
+    static Textures tt;
+    static Panel panel;
+    static int originalWidth; //stays the same
+    static int originalHeight; //stays the same
+    static int frameWidth;
+    static int frameHeight;
+    static int frameExtraWidth;
+    static int frameExtraHeight;
+    static String title;
+    static String version;
+    static boolean fullScreen;
+    static boolean stretched;
+    static Toolkit tk = Toolkit.getDefaultToolkit();
+
+    public Window(){
+        //change ui for jFileChooser + other UI settings
+        try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
+        catch (ClassNotFoundException e) { e.printStackTrace(); }
+        catch (InstantiationException e) { e.printStackTrace(); }
+        catch (IllegalAccessException e) { e.printStackTrace(); }
+        catch (UnsupportedLookAndFeelException e) { e.printStackTrace(); }
+        //construct frame and panel
+        tt = new Textures();
+        jf = new JFrame();
+        originalWidth  = 320;
+        originalHeight = 240;
+        //stretched = true;
+        panel = new Panel();
+        panel.setPreferredSize(new Dimension(originalWidth, originalHeight));
+        title = "Hitch and Chapstick - Doppler Indie Games";
+        version = "v0.1";
+        //build window
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setIconImage(tt.icon);
+        jf.setTitle(title+" ["+version+"]");
+        jf.add(panel);
+        jf.setResizable(true);
+        jf.pack();
+        jf.setMinimumSize(new Dimension(jf.getWidth(),jf.getHeight()));
+        jf.setLocationRelativeTo(null);
+        jf.requestFocusInWindow();
+        jf.setVisible(true);
+        //update extras
+        frameExtraWidth = jf.getInsets().left + jf.getInsets().right;
+        frameExtraHeight = jf.getInsets().top + jf.getInsets().bottom;
+        //start fullscreen
+        toggleFullScreen();
+        toggleStretched();
+    }
+    public static int getOriginalWidth() { return originalWidth; }
+    public static int getOriginalHeight(){ return originalHeight;}
+    public static int getFrameWidth(){ return frameWidth-frameExtraWidth; }
+    public static int getFrameHeight(){ return frameHeight-frameExtraHeight; }
+    public static boolean isStretched(){ return stretched; }
+    public static void toggleStretched(){ stretched = !stretched; }
+    public static void toggleFullScreen(){
+        if (fullScreen){ //set windowed
+            jf.dispose();
+            jf.setUndecorated(false);
+            jf.setVisible(true);
+            frameWidth  = originalWidth;
+            frameHeight = originalHeight;
+            fullScreen = false;
+            jf.setPreferredSize(new Dimension(frameWidth, frameHeight));
+            jf.pack();
+            jf.setLocationRelativeTo(null);
+        }
+        else{
+            jf.dispose();
+            jf.setUndecorated(true);
+            jf.setVisible(true);
+            frameWidth = (int)tk.getScreenSize().getWidth();
+            frameHeight = (int)tk.getScreenSize().getHeight();
+            fullScreen = true;
+            jf.setPreferredSize(new Dimension(frameWidth+frameExtraWidth, frameHeight+frameExtraHeight));
+            jf.pack();
+            jf.setLocation(new Point(0, 0));
+        }
+    }
+    public static void updateSize(){
+        frameWidth = jf.getWidth();
+        frameHeight = jf.getHeight();
+    }
+}
